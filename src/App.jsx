5244252a10e6a1473732e41adeecf8ac59e5def8
Kim CG <cjfrbs1256@gmail.com>
2024-01-23 11:24:45 +0900

    complete

---
+++ src/App.jsx
@@ -0,0 +1,117 @@
+import React, { useRef, useState } from "react";
+import "./App.css";
+import "./css/todo.css";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const id = useRef(0);
+
+  const onCreate = (title, contents, isDone) => {
+    const newItem = {
+      title,
+      contents,
+      isDone,
+      id: id.current,
+    };
+    id.current += 1;
+    setTodos([...todos, newItem]);
+  };
+
+  const [title, setTitle] = useState("");
+  const [contents, setContents] = useState("");
+
+  const titleHandler = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const contentHandler = (e) => {
+    setContents(e.target.value);
+  };
+
+  const todoButton = () => {
+    const add = {
+      title,
+      contents,
+      isDone: false,
+    };
+    setTodos([...todos, add]);
+    setTitle("");
+    setContents("");
+  };
+
+  const completeButton = (index) => {
+    const completeTodo = todos[index]; //클릭된 state의 index값
+    const doneState = todos.filter((value, i) => i !== index);
+    completeTodo.isDone = true;
+    setTodos([...doneState, completeTodo]);
+  };
+
+  const cancelButton = (index) => {
+    const cancelTodo = todos[index];
+    cancelTodo.isDone = false;
+    const cancelState = todos.filter((value, i) => i !== index);
+    setTodos([...cancelState, cancelTodo]);
+  };
+
+  return (
+    <div className="body">
+      <div className="header">
+        <p>Todo-List</p>
+        <p>React</p>
+      </div>
+      <div className="addList">
+        <div>
+          <strong>제목</strong> :
+          <input value={title} onChange={titleHandler} className="addInput" />
+          <strong>내용</strong> :
+          <input
+            value={contents}
+            onChange={contentHandler}
+            className="addInput"
+          />
+        </div>
+        <button onClick={todoButton}>추가하기</button>
+      </div>
+      <div className="contentBox">
+        <h4>Working..</h4>
+        <div className="workingBox">
+          {todos
+            .filter((item) => item.isDone === false)
+            .map((item, index) => {
+              return (
+                <div className="todoItem" key={index}>
+                  <p>{item.title}</p>
+                  <p>{item.contents}</p>
+                  <button className="deleteBtn">삭제하기</button>
+                  <button
+                    onClick={() => completeButton(index)}
+                    className="completeBtn"
+                  >
+                    완료
+                  </button>
+                </div>
+              );
+            })}
+        </div>
+        <h4>Done...!</h4>
+        <div className="doneBox">
+          {todos
+            .filter((item) => item.isDone === true)
+            .map((item, index) => {
+              return (
+                <div className="todoItem" key={index}>
+                  <p>{item.title}</p>
+                  <p>{item.contents}</p>
+                  <button className="deleteBtn">삭제하기</button>
+                  <button onClick={() => cancelButton(index)}>취소</button>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/TodoList.jsx
@@ -0,0 +1,16 @@
+// const TodoList = (data) => {
+//   return (
+//     <div className="contentBox">
+//       <div className="workingBox">
+//         <h4>Working..</h4>
+//         {data.map((item) => {
+//           return <div></div>;
+//         })}
+//       </div>
+//       <div className="doneBox">
+//         <h4>Done...!</h4>
+//       </div>
+//     </div>
+//   );
+// };
+// export default TodoList;
